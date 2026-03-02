2d74869a1f18baec2ccb59f61acd8f7db06b53f4
Khalid Farooqi <k.farooqi@live.de>
2026-03-02 19:40:51 +0100

    test: add GoogleTest setup with MovingAverage unit tests. add Makefile

---
+++ build_tests/run_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "C:/Users/Khalid/Documents/PlatformIO/Projects/SensorNode-AVR/build_tests/run_tests[1]_tests.cmake")
+  include("C:/Users/Khalid/Documents/PlatformIO/Projects/SensorNode-AVR/build_tests/run_tests[1]_tests.cmake")
+else()
+  add_test(run_tests_NOT_BUILT run_tests_NOT_BUILT)
+endif()
